e86d58bb865f7fd368dea394904578ee6793801f
akash35876 <ak35876@gmail.com>
2023-02-16 18:26:48 +0530

    Changed

--- javaProject/src/main/java/com/example/javaProject/JavaProjectApplication.java
+++ javaProject/src/main/java/com/example/javaProject/JavaProjectApplication.java
@@ -8,6 +8,8 @@ public class JavaProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaProjectApplication.class, args);
+		
+		System.out.println("Application Started");
 	}
 
 }
