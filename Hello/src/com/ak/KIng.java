b1888d69a19c64bc1c3850048c932c0ba1be621c
akash35876 <ak35876@gmail.com>
2023-11-20 23:35:49 +0530

    Pojo model

--- Hello/src/com/ak/KIng.java
+++ Hello/src/com/ak/KIng.java
@@ -3,14 +3,56 @@ package com.ak;
 public class KIng {
 
 	public static void main(String[] args) {
-		int i=10;
-		int k=20;
-		for(int j=0; j<=i; j++) {
-			for(int l=5; l<=j;l++) {
-				System.out.println(l);
-			}
+		 
+		Heelo h = new Heelo();		
+		h.setAge("Akash");
+		h.setMonth("November");
+		h.setName("AKSh");
+		h.setSno("1");
+		 System.out.println(h.toStringValues());
+	}
+	
+	static class Heelo{
+		private String sno;
+		private String name;
+		private String age;
+		private String month;
+		public Heelo(String string, String string2, String string3, String string4) {
+			 
 		}
-
+		public Heelo() {
+			// TODO Auto-generated constructor stub
+		}
+		public String getSno() {
+			return sno;
+		}
+		public void setSno(String sno) {
+			this.sno = sno;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getAge() {
+			return age;
+		}
+		public void setAge(String age) {
+			this.age = age;
+		}
+		public String getMonth() {
+			return month;
+		}
+		public void setMonth(String month) {
+			this.month = month;
+		}
+		 public String toStringValues() {
+	            return "sno='" + sno + '\'' +
+	                    ", name='" + name + '\'' +
+	                    ", age='" + age + '\'' +
+	                    ", month='" + month + '\'';
+	        }
 	}
 
 }
