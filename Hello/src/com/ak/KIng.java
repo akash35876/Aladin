fcef4ad703de845c30bae12b5b5913680aa07942
akash35876 <ak35876@gmail.com>
2023-11-20 23:03:27 +0530

    LOOP

--- Hello/src/com/ak/KIng.java
+++ Hello/src/com/ak/KIng.java
@@ -3,9 +3,13 @@ package com.ak;
 public class KIng {
 
 	public static void main(String[] args) {
-		System.out.println("AKash");
-		System.out.println("Dhiva");
-		System.out.println("Hello");
+		int i=10;
+		int k=20;
+		for(int j=0; j<=i; j++) {
+			for(int l=5; l<=j;l++) {
+				System.out.println(l);
+			}
+		}
 
 	}
 
